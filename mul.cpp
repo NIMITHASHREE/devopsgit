0c2057928fdc2fcb3acdf71f42b03b86a2455b25
Nimithashree <nimithashreea@gmail.com>
2026-07-15 09:10:33 +0000

    commit mul

---
+++ mul.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int a,b;
+        cout<<"enter two numbes:\n";
+       cin>>a>>b;
+        cout<<"multiplication:"<<a*b;
+        cout<<"\ndivision:"<<a/b;
+        return 0;
+}
+
