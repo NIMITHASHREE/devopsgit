2a2b5292aab61805b54811fba526f5673c0c7863
Nimithashree <nimithashreea@gmail.com>
2026-07-15 08:59:36 +0000

    spot

---
+++ calc.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter two numbes:\n";
+       cin>>a>>b;
+        cout<<"addition:"<<a+b;
+ 	cout<<"subtraction:"<<a-b;
+	return 0;
+}	
